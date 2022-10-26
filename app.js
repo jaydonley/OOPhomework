22dbe2764f319e785c9345d40cc80bb68ebda9a4
Jason Donley <jay.donley@gmail.com>
2022-10-26 10:44:17 -0400

    updated last problem

--- app.js
+++ app.js
@@ -54,4 +54,43 @@ let dog = {
 
   //-----------------------------
 
+  function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+  }
   
+  // Only change code below this line
+  
+  let myHouse ={
+    numBedrooms: 4
+  };
+  
+  myHouse instanceof House;
+
+  //-----------------------------------
+
+  function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  // Only change code below this line
+  
+  for (let property in Bird){
+    if(canary.hasOwnProperty(property)){
+      own.Props.push(property);
+    }
+  }
+  console.log(ownProps);
+
+  //--------------------------------
+
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  // Only change code above this line
+  let beagle = new Dog("Snoopy");
